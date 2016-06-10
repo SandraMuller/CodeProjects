392812d82076202371ebbe0ac227cee5bdb2f5be
Sandra <mursan03@gmail.com>
2016-06-10 14:09:20 +1200

    UnitTestingProject
    Project to work on unit testing

---
+++ Calculator/Calculator.Tests/SimpleCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class SimpleCalculatorTests
+    {
+        [Test]
+        public void shouldAddTwoNumbers()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
